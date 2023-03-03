f2541c439a1aba95b1a8797c9b808bbcd833d42d
Harsh Shah <shah.harsh7@northeastern.edu>
2023-03-02 23:11:35 -0500

    added CLI command for get geos files

--- FastAPI/cli.py
+++ FastAPI/cli.py
@@ -1,8 +1,18 @@
 import typer
+import httpx
 import requests
-from Functions.auth import AuthHandler
 from pydantic import BaseModel
 from bcrypt import hashpw, gensalt
+from Functions.auth import AuthHandler
+from Functions.schema import LoginDetails, RegisterDetails, ChangePasswordDetails
+from Functions.database import check_username_taken, register_user, get_password, login_user, log_data, verify_token, call_counter, verify_rate_limit
+from Functions.database import reset_calls, verify_admin, admin_data, update_password,user_data
+from Functions.extractFiles import extract_files
+from Functions.uploadFileToS3 import upload_file_to_s3
+from fastapi import Request, HTTPException
+
+from Functions.urlGen import geos_url_gen
+from Functions.urlGen import nexrad_url_gen
 
 from Functions.extractFiles import extract_files
 
@@ -71,6 +81,89 @@ def change_password(username: str, current_password: str, new_password: str):
 
 
 
+
+
+
+
+@app.command()
+def geos_get_files_path(
+    username: str = typer.Argument(..., help="The username of the user making the request"),
+    station: str = typer.Argument(..., help="The station of the files to be extracted"),
+    year: str = typer.Argument(..., help="The year of the files to be extracted"),
+    day: str = typer.Argument(..., help="The day of the files to be extracted"),
+    hour: str = typer.Argument(..., help="The hour of the files to be extracted"),
+):
+    """
+    Extracts files based on the provided parameters.
+    """
+    # Define endpoint URL
+    endpoint = f"http://localhost:8000/geos-get-by-path/{username}/{station}/{year}/{day}/{hour}"
+
+    # Send HTTP GET request to endpoint
+    response = httpx.get(endpoint)
+
+    # Check response status code
+    if response.status_code == 200:
+        typer.echo(response.text)
+    else:
+        typer.echo(f"Error: {response.status_code} - {response.text}")
+
+
+
+
+
+
+
+
+"""
+@app.command()
+def geos_get_files_path(
+    username: str = typer.Argument(..., help="The username of the user making the request"),
+    station: str = typer.Argument(..., help="The station of the files to be extracted"),
+    year: str = typer.Argument(..., help="The year of the files to be extracted"),
+    day: str = typer.Argument(..., help="The day of the files to be extracted"),
+    hour: str = typer.Argument(..., help="The hour of the files to be extracted"),
+):
+  
+    # Import required modules
+
+    # Define endpoint
+    endpoint = f"/geos-get-by-path/{username}/{station}/{year}/{day}/{hour}"
+
+    # Verify if plan rate is exceeded
+    if not verify_rate_limit(username):
+        log_data(username, endpoint, call_status=429)
+        raise HTTPException(status_code=429, detail='API rate limit exceeded! Upgrade plan to continue')
+
+    # Get user's IP address
+    ip = Request().client.host
+
+    # Check token validity and IP address
+    if verify_token(username, ip):
+        # Extract all files
+        path = f"{station}/{year}/{day}/{hour}/"
+        result = extract_files("noaa-goes18", path)
+
+        # Check if the result is valid, if not it raises an error
+        if not result:
+            # Log error
+            log_data(username, endpoint, call_status=400)
+            raise HTTPException(status_code=400, detail='Path parameters not valid')
+
+        # Log successful call
+        log_data(username, endpoint, call_status=201)
+
+        # Increment API call counter
+        call_counter(username)
+
+        return result
+
+
+
+
+
+
+
 @app.command()
 def geos_get_files_path(username: str, station: str, year: int, day: int, hour: int):
     url = f"{base_url}/geos-get-by-path/{username}/{station}/{year}/{day}/{hour}"
@@ -84,6 +177,8 @@ def geos_get_files_path(username: str, station: str, year: int, day: int, hour: 
     else:
         typer.echo(response.json()["detail"])
 
+
+"""
 
 
 @app.command()
